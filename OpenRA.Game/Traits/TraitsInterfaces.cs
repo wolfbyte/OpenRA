be0848dbec9b48afa380089bfc9a2e169b252480
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2017-09-10 21:16:08 +0300

    Apply some updates forcecore did

--- OpenRA.Game/Traits/TraitsInterfaces.cs
+++ OpenRA.Game/Traits/TraitsInterfaces.cs
@@ -458,10 +458,4 @@ namespace OpenRA.Traits
 			return playerName + " " + BoolValues[newValue] + " " + Name + ".";
 		}
 	}
-
-	public interface INotifyDeploy
-	{
-		void OnDeployed(Actor self);
-		void OnUndeployed(Actor self);
-	}
 }
