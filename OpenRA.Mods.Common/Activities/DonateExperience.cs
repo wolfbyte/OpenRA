ff1bcc513f38cc3df5b3736dd2cdc961dcbe4492
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2017-09-14 08:59:20 +0300

    Update DeliversExperience

--- OpenRA.Mods.Common/Activities/DonateExperience.cs
+++ OpenRA.Mods.Common/Activities/DonateExperience.cs
@@ -10,13 +10,7 @@
 #endregion
 
 using OpenRA.Activities;
-<<<<<<< HEAD
 using OpenRA.Mods.Common.Traits;
-=======
-using OpenRA.Mods.Common.Effects;
-using OpenRA.Mods.Common.Traits;
-using OpenRA.Traits;
->>>>>>> Upload Engine for Generals Alpha
 
 namespace OpenRA.Mods.Common.Activities
 {
@@ -28,7 +22,7 @@ namespace OpenRA.Mods.Common.Activities
 		readonly int playerExperience;
 
 		public DonateExperience(Actor self, Actor target, int level, int playerExperience, GainsExperience targetGainsExperience)
-			: base(self, target, EnterBehaviour.Dispose)
+			: base(self, target, EnterBehaviour.Dispose, WDist.Zero)
 		{
 			this.target = target;
 			this.level = level;
