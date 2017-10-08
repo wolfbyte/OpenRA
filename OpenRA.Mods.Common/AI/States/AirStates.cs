86c7b623183eefb98d1256c3fe0682cf4fab72e6
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2017-10-08 13:45:57 +0300

    Fix a crash with AirStates

--- OpenRA.Mods.Common/AI/States/AirStates.cs
+++ OpenRA.Mods.Common/AI/States/AirStates.cs
@@ -312,6 +312,9 @@ namespace OpenRA.Mods.Common.AI
 
 		public void Activate(Squad owner)
 		{
+			if (!owner.IsValid)
+				return;
+
 			escapeDest = RandomBuildingLocation(owner);
 			safePoint = CalcSafePoint(owner, escapeDest, EnemyStaticAAs(owner));
 		}
