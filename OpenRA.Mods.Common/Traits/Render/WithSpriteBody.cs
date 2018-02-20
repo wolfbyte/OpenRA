8e9e625f4068d8c33791a0d707939a3b158e92d2
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-02-20 23:00:55 +0300

    Some fixes to WithTerrainDependantSpriteBody

--- OpenRA.Mods.Common/Traits/Render/WithSpriteBody.cs
+++ OpenRA.Mods.Common/Traits/Render/WithSpriteBody.cs
@@ -47,7 +47,7 @@ namespace OpenRA.Mods.Common.Traits.Render
 	public class WithSpriteBody : PausableConditionalTrait<WithSpriteBodyInfo>, INotifyDamageStateChanged, INotifyBuildComplete, IAutoMouseBounds
 	{
 		public readonly Animation DefaultAnimation;
-		readonly RenderSprites rs;
+		public readonly RenderSprites rs;
 		readonly Animation boundsAnimation;
 
 		public WithSpriteBody(ActorInitializer init, WithSpriteBodyInfo info)
