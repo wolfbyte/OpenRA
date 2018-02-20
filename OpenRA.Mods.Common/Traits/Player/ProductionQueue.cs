68b4856a4382f3f2a07cccc975a9470523801e64
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-02-20 20:56:06 +0300

    Fix the issue with capturing a driver killed CV.

--- OpenRA.Mods.Common/Traits/Player/ProductionQueue.cs
+++ OpenRA.Mods.Common/Traits/Player/ProductionQueue.cs
@@ -101,6 +101,9 @@ namespace OpenRA.Mods.Common.Traits
 		public string Faction { get; private set; }
 		[Sync] public bool IsValidFaction { get; private set; }
 
+		bool anyEnabledProduction = false;
+		bool anyUnpausedProduction = false;
+
 		public ProductionQueue(ActorInitializer init, Actor playerActor, ProductionQueueInfo info)
 		{
 			self = init.Self;
@@ -252,16 +255,30 @@ namespace OpenRA.Mods.Common.Traits
 		protected virtual void Tick(Actor self)
 		{
 			// PERF: Avoid LINQ when checking whether all production traits are disabled/paused
-			var anyEnabledProduction = false;
-			var anyUnpausedProduction = false;
+			var wasDisabled = !anyEnabledProduction;
+
+			anyEnabledProduction = false;
+			anyUnpausedProduction = false;
 			foreach (var p in productionTraits)
 			{
 				anyEnabledProduction |= !p.IsTraitDisabled;
 				anyUnpausedProduction |= !p.IsTraitPaused;
 			}
 
+			if (wasDisabled && anyEnabledProduction)
+			{
+				self.World.AddFrameEndTask(_ =>
+				{
+					self.Owner.PlayerActor.Trait<TechTree>().Update();
+					CacheProducibles(self.Owner.PlayerActor);
+				});
+			}
+
 			if (!anyEnabledProduction)
+			{
+				self.Owner.PlayerActor.Trait<TechTree>().Remove(this);
 				ClearQueue();
+			}
 
 			Enabled = IsValidFaction && anyEnabledProduction;
 
