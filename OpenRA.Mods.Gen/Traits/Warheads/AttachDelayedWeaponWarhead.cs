c54f86d36303cae142762861ec7e0bd94ca7d2a1
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-02-26 18:42:09 +0300

    Changes to do with rebase.

--- OpenRA.Mods.Gen/Traits/Warheads/AttachDelayedWeaponWarhead.cs
+++ OpenRA.Mods.Gen/Traits/Warheads/AttachDelayedWeaponWarhead.cs
@@ -50,7 +50,7 @@ namespace OpenRA.Mods.Yupgi_alert.Warheads
 			if (!IsValidImpact(pos, firedBy))
 				return;
 
-			var availableActors = firedBy.World.FindActorsInCircle(pos, Range + VictimScanRadius);
+			var availableActors = firedBy.World.FindActorsInCircle(pos, Range);
 			foreach (var actor in availableActors)
 			{
 				if (!IsValidAgainst(actor, firedBy))
