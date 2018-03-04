43b8a88e809c1530445a291adcbddf84608736ac
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-03-04 17:35:18 +0300

    Add a bool to not show Refund text for Sellable:

--- OpenRA.Mods.Common/Traits/Sellable.cs
+++ OpenRA.Mods.Common/Traits/Sellable.cs
@@ -24,6 +24,7 @@ namespace OpenRA.Mods.Common.Traits
 		public readonly int RefundPercent = 50;
 		public readonly string[] SellSounds = { };
 		public readonly bool ShowTicks = true;
+		public readonly bool ShowTooltipText = true;
 
 		[Desc("Skip playing (reversed) make animation.")]
 		public readonly bool SkipMakeAnimation = false;
@@ -102,7 +103,7 @@ namespace OpenRA.Mods.Common.Traits
 
 		public bool IsTooltipVisible(Player forPlayer)
 		{
-			if (!IsTraitDisabled && self.World.OrderGenerator is SellOrderGenerator)
+			if (info.ShowTooltipText && !IsTraitDisabled && self.World.OrderGenerator is SellOrderGenerator)
 				return forPlayer == self.Owner;
 			return false;
 		}
