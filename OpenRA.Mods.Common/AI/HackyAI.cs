70c755aee398f331a503dc22d4ef8326efc2779e
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-03-15 11:22:21 +0300

    AI now ignores harvesters in ProductionMinimumCash logic.

--- OpenRA.Mods.Common/AI/HackyAI.cs
+++ OpenRA.Mods.Common/AI/HackyAI.cs
@@ -1348,7 +1348,7 @@ namespace OpenRA.Mods.Common.AI
 		void ProductionUnits(Actor self)
 		{
 			// Stop building until economy is restored
-			if (!HasAdequateProc() || Info.ProductionMinimumCash > playerResource.Cash)
+			if (!HasAdequateProc())
 				return;
 
 			// No construction yards - Build a new MCV
@@ -1377,6 +1377,9 @@ namespace OpenRA.Mods.Common.AI
 				ChooseUnitToBuild(queue);
 
 			if (unit == null)
+				return;
+
+			if (unit.TraitInfoOrDefault<HarvesterInfo>() == null && Info.ProductionMinimumCash > playerResource.Cash)
 				return;
 
 			var name = unit.Name;
