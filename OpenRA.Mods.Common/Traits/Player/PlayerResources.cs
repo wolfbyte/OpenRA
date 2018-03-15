fda5350690e8de1d271129a08ffd5373bdd363dd
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-03-15 20:22:36 +0300

    Forgot to update changes in PlayerResources
    Makes PlayerResources -> Info public

--- OpenRA.Mods.Common/Traits/Player/PlayerResources.cs
+++ OpenRA.Mods.Common/Traits/Player/PlayerResources.cs
@@ -61,12 +61,12 @@ namespace OpenRA.Mods.Common.Traits
 
 	public class PlayerResources : ITick, ISync
 	{
-		readonly PlayerResourcesInfo info;
+		public readonly PlayerResourcesInfo Info;
 		readonly Player owner;
 
 		public PlayerResources(Actor self, PlayerResourcesInfo info)
 		{
-			this.info = info;
+			this.Info = info;
 			owner = self.Owner;
 
 			var startingCash = self.World.LobbyInfo.GlobalSettings
@@ -164,11 +164,11 @@ namespace OpenRA.Mods.Common.Traits
 		{
 			if (Cash + Resources < num)
 			{
-				if (notifyLowFunds && !string.IsNullOrEmpty(info.InsufficientFundsNotification) &&
-					owner.World.WorldTick - lastNotificationTick >= info.InsufficientFundsNotificationDelay)
+				if (notifyLowFunds && !string.IsNullOrEmpty(Info.InsufficientFundsNotification) &&
+					owner.World.WorldTick - lastNotificationTick >= Info.InsufficientFundsNotificationDelay)
 				{
 					lastNotificationTick = owner.World.WorldTick;
-					Game.Sound.PlayNotification(owner.World.Map.Rules, owner, "Speech", info.InsufficientFundsNotification, owner.Faction.InternalName);
+					Game.Sound.PlayNotification(owner.World.Map.Rules, owner, "Speech", Info.InsufficientFundsNotification, owner.Faction.InternalName);
 				}
 
 				return false;
