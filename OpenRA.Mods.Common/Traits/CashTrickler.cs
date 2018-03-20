6b4c2c0f14ecb4121044ec1cf2cc9d6328177a06
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-03-20 12:13:24 +0300

    Add logic for Fake Cash Trickler
    for Fake Black Market

--- OpenRA.Mods.Common/Traits/CashTrickler.cs
+++ OpenRA.Mods.Common/Traits/CashTrickler.cs
@@ -28,6 +28,9 @@ namespace OpenRA.Mods.Common.Traits
 		[Desc("Amount of money to give each time.")]
 		public readonly int Amount = 15;
 
+		[Desc("Just shows fake cash ticks and not actually give cash.")]
+		public readonly bool Fake = false;
+
 		[Desc("Whether to show the cash tick indicators rising from the actor.")]
 		public readonly bool ShowTicks = true;
 
@@ -91,14 +94,17 @@ namespace OpenRA.Mods.Common.Traits
 			{
 				// Check whether the amount of cash to be removed would exceed available player cash, in that case only remove all the player cash
 				var drain = Math.Min(resources.Cash + resources.Resources, -amount);
-				resources.TakeCash(drain);
+				if (!info.Fake)
+					resources.TakeCash(drain);
 
 				if (info.ShowTicks)
 					AddCashTick(self, -drain);
 			}
 			else
 			{
-				resources.GiveCash(amount);
+				if (!info.Fake)
+					resources.GiveCash(amount);
+
 				if (info.ShowTicks)
 					AddCashTick(self, amount);
 			}
