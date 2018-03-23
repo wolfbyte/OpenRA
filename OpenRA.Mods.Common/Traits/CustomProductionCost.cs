c8aa01faa0cd6d02a41ea2ad72838a3bddb049f2
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-03-23 09:32:36 +0300

    Add a logic to give actors custom production cost per queue.
    Duplicate actors are getting out of hand.

---
+++ OpenRA.Mods.Common/Traits/CustomProductionCost.cs
@@ -0,0 +1,30 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Changes the production cost of this actor for a specific queue.")]
+	public class CustomProductionCostInfo : TraitInfo<CustomProductionCost>
+	{
+		[FieldLoader.Require]
+		[Desc("Custom production cost for the unit.")]
+		public readonly int Cost = 0;
+
+		[FieldLoader.Require]
+		[Desc("Queues that this cost will apply.")]
+		public readonly HashSet<string> Queue = new HashSet<string>();
+	}
+
+	public class CustomProductionCost { }
+}
