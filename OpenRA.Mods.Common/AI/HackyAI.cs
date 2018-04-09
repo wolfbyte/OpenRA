556bca878aca1e922f5e3936657e1885015cb1fd
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-04-09 08:48:03 +0300

    Rebase stuff.

--- OpenRA.Mods.Common/AI/HackyAI.cs
+++ OpenRA.Mods.Common/AI/HackyAI.cs
@@ -314,6 +314,7 @@ namespace OpenRA.Mods.Common.AI
 		CPos initialBaseCenter;
 		PowerManager playerPower;
 		PlayerResources playerResource;
+		ResourceLayer resLayer;
 		int ticks;
 
 		BitArray resourceTypeIndices;
@@ -403,6 +404,7 @@ namespace OpenRA.Mods.Common.AI
 			IsEnabled = true;
 			playerPower = p.PlayerActor.TraitOrDefault<PowerManager>();
 			playerResource = p.PlayerActor.Trait<PlayerResources>();
+			resLayer = World.WorldActor.TraitOrDefault<ResourceLayer>();
 
 			harvManager = new AIHarvesterManager(this, p);
 			supportPowerManager = new AISupportPowerManager(this, p);
