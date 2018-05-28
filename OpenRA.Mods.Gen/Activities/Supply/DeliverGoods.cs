3e0f01dc95679af1e253a0460c2e4a14db41c630
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-05-28 15:31:16 +0300

    Add Generals Supply Logic

---
+++ OpenRA.Mods.Gen/Activities/Supply/DeliverGoods.cs
@@ -0,0 +1,127 @@
+﻿using System.Drawing;
+using System.Linq;
+using OpenRA.Activities;
+using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Effects;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Traits.Render;
+using OpenRA.Mods.Yupgi_alert.Traits;
+using OpenRA.Support;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Yupgi_alert.Activities
+{
+    class DeliverGoods : Activity
+    {
+        readonly SupplyCollector collector;
+		readonly SupplyCollectorInfo collectorInfo;
+        readonly IMove move;
+		readonly Mobile mobile;
+		readonly IPathFinder pathFinder;
+
+		public DeliverGoods(Actor self)
+		{
+			collector = self.Trait<SupplyCollector>();
+			collectorInfo = self.Info.TraitInfo<SupplyCollectorInfo>();
+            move = self.Trait<IMove>();
+			mobile = self.TraitOrDefault<Mobile>();
+			pathFinder = self.World.WorldActor.Trait<IPathFinder>();
+		}
+
+        public override Activity Tick(Actor self)
+        {
+            if (IsCanceled || NextActivity != null)
+                return NextActivity;
+
+            if (collector.deliveryBuilding == null || !collector.deliveryBuilding.IsInWorld)
+            {
+				collector.deliveryBuilding = collector.ClosestDeliveryBuilding(self);
+            }
+
+            if (collector.deliveryBuilding == null || !collector.deliveryBuilding.IsInWorld)
+            {
+                return ActivityUtils.SequenceActivities(new Wait(collectorInfo.SearchForDeliveryBuildingDelay), this);
+            }
+
+			var center = collector.deliveryBuilding;
+			self.SetTargetLine(Target.FromActor(center), Color.Green, false);
+
+			CPos cell;
+			var random = new MersenneTwister();
+			var centerTrait = center.Trait<SupplyCenter>();
+			if (mobile != null)
+				cell = self.ClosestCell(centerTrait.Info.DeliveryOffsets.Where(c => mobile.CanEnterCell(center.Location + c)).Select(c => center.Location + c));
+			else
+				cell = self.ClosestCell(centerTrait.Info.DeliveryOffsets.Select(c => center.Location + c));
+
+			if (!centerTrait.Info.DeliveryOffsets.Select(o => center.Location + o).Contains(self.Location))
+            {
+                return ActivityUtils.SequenceActivities(move.MoveTo(cell, 2), this);
+            }
+
+			if (self.Trait<IFacing>() != null)
+			{
+				if (centerTrait.Info.Facing >= 0 && self.Trait<IFacing>().Facing != centerTrait.Info.Facing)
+				{
+					return ActivityUtils.SequenceActivities(new Turn(self, centerTrait.Info.Facing), this);
+				}
+				else if (centerTrait.Info.Facing == -1)
+				{
+					var facing = (center.CenterPosition - self.CenterPosition).Yaw.Facing;
+					if (self.Trait<IFacing>().Facing != facing)
+					{
+						return ActivityUtils.SequenceActivities(new Turn(self, facing), this);
+					}
+				}
+			}
+
+			if (!collector.Waiting)
+			{
+				collector.Waiting = true;
+				return ActivityUtils.SequenceActivities(new Wait(collectorInfo.DeliveryDelay), this);
+			}
+
+			var amount = collector.Amount;
+			if (amount < 0)
+				return new FindGoods(self);
+			
+			if (centerTrait.CanGiveResource(amount))
+            {
+				var wsb = self.TraitsImplementing<WithSpriteBody>().Where(t => !t.IsTraitDisabled).FirstOrDefault();
+				var wsda = self.Info.TraitInfoOrDefault<WithSupplyDeliveryAnimationInfo>();
+				var rs = self.TraitOrDefault<RenderSprites>();
+				if (rs != null && wsb != null && wsda != null && !collector.DeliveryAnimPlayed)
+				{
+					wsb.PlayCustomAnimation(self, wsda.DeliverySequence);
+					collector.DeliveryAnimPlayed = true;
+					return ActivityUtils.SequenceActivities(new Wait(wsda.WaitDelay), this);
+				}
+
+				var wsdo = self.TraitOrDefault<WithSupplyDeliveryOverlay>();
+				if (wsb != null && wsdo != null && !collector.DeliveryAnimPlayed)
+				{
+					if (!wsdo.Visible)
+					{
+						wsdo.Visible = true;
+						wsdo.Anim.PlayThen(wsdo.Info.Sequence, () => wsdo.Visible = false);
+						collector.DeliveryAnimPlayed = true;
+						var sequence = wsb.DefaultAnimation.GetSequence(wsdo.Info.Sequence);
+						return ActivityUtils.SequenceActivities(new Wait(wsdo.Info.WaitDelay), this);
+					}
+				}
+
+				collector.Waiting = false;
+				collector.DeliveryAnimPlayed = false;
+				centerTrait.GiveResource(amount, self.Info.Name);
+
+				collector.Amount = 0;
+				collector.CheckConditions(self);
+			}
+			else
+				return ActivityUtils.SequenceActivities(new Wait(collectorInfo.DeliveryDelay), this);
+
+			return new FindGoods(self);
+        }
+    }
+}
---
+++ OpenRA.Mods.Gen/Traits/Supply/SupplyDock.cs
@@ -0,0 +1,100 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+ 
+using System.Collections.Generic;
+using OpenRA.Traits;
+using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.Yupgi_alert.Traits
+{
+	public class SupplyDockInfo : ITraitInfo
+	{
+		public readonly HashSet<string> SupplyTypes = new HashSet<string> { "supply" };
+
+		[Desc("How much supplies this actor carries.")]
+		public readonly int Capacity = 30000;
+
+		[FieldLoader.Require]
+		[Desc("Where can the supply collectors can collect the supplies from.")]
+		public readonly CVec[] CollectionOffsets = new CVec[] { };
+		
+		[Desc("Where can the aircraft supply collectors can collect the supplies from.")]
+		public readonly CVec[] AircraftCollectionOffsets = new CVec[] { };
+
+		[Desc("Collector faces this way before taking the supplies; if -1, faces towards the center of dock.")]
+		public readonly int Facing = -1;
+		
+		[Desc("Conditions to grant when dock has more than specified amount of supplies.",
+			"A dictionary of [integer]: [condition].")]
+		public readonly Dictionary<int, string> FullnessConditions = new Dictionary<int, string>();
+
+		[GrantedConditionReference]
+		public IEnumerable<string> LinterFullnessConditions { get { return FullnessConditions.Values; } }
+
+		public object Create(ActorInitializer init) { return new SupplyDock(init.Self, this); }
+	}
+
+	public class SupplyDock : IProvideTooltipInfo, INotifyCreated
+	{
+		readonly Actor self;
+		public readonly SupplyDockInfo Info;
+		public int Amount;
+
+		ConditionManager conditionManager;
+		readonly Dictionary<int, int> fullnessTokens = new Dictionary<int, int>();
+
+		public SupplyDock(Actor self, SupplyDockInfo info)
+		{
+			this.self = self;
+			Info = info;
+			Amount = Info.Capacity;
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			conditionManager = self.TraitOrDefault<ConditionManager>();
+			CheckConditions(self);
+		}
+
+		public bool IsFull { get { return Amount == Info.Capacity; } }
+		public bool IsEmpty { get { return Amount == 0; } }
+		public int Fullness { get { return Amount * 100 / Info.Capacity; } }
+
+		public void CheckConditions(Actor self)
+		{
+			if (conditionManager != null)
+			{
+				foreach (var pair in Info.FullnessConditions)
+				{
+					if (Amount >= pair.Key && !fullnessTokens.ContainsKey(pair.Key))
+						fullnessTokens.Add(pair.Key, conditionManager.GrantCondition(self, pair.Value));
+
+					int fullnessToken;
+					if (Amount < pair.Key && fullnessTokens.TryGetValue(pair.Key, out fullnessToken))
+					{
+						conditionManager.RevokeCondition(self, fullnessToken);
+						fullnessTokens.Remove(pair.Key);
+					}
+				}
+			}
+		}
+	
+		public bool IsTooltipVisible(Player forPlayer) { return true; }
+
+		public string TooltipText
+		{
+			get
+			{
+				return "$" + Amount;
+			}
+		}
+	}
+}
