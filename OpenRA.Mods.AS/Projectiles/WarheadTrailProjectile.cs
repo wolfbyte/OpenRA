4e4578d8aeead31545520401325238a41a5f0ad7
Zimmermann Gyula <graiondilach@hotmail.com>
2018-11-06 11:10:04 +0100

    Misc. fixes.
    Fixes FireRadiusWarhead/FireReverseRadiusWarhead spread calculation.
    Fixes a crash if WarheadTrailProjectile is used as a secondary stage weapon.
    Fixes WarheadTrailProjectile ignoring firing offsets.

--- OpenRA.Mods.AS/Projectiles/WarheadTrailProjectile.cs
+++ OpenRA.Mods.AS/Projectiles/WarheadTrailProjectile.cs
@@ -225,17 +225,18 @@ namespace OpenRA.Mods.AS.Projectiles
 				};
 
 				projectiles[i] = new WarheadTrailProjectileEffect(info, projectileArgs, lifespan, estimatedLifespan);
-				world.Add(projectiles[i]);
 			}
+
+			foreach (var p in projectiles)
+				world.AddFrameEndTask(w => w.Add(p));
 		}
 
 		// gets where main projectile should fly to
 		WPos GetTargetPos()
 		{
 			var targetpos = args.PassiveTarget;
-			var actorpos = args.SourceActor.CenterPosition;
 
-			return WPos.Lerp(actorpos, targetpos, args.Weapon.Range.Length, (targetpos - actorpos).Length);
+			return WPos.Lerp(sourcepos, targetpos, args.Weapon.Range.Length, (targetpos - sourcepos).Length);
 		}
 
 		public void Tick(World world)
